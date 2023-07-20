f1da11f74426b5299347f718d748d4e869ecf717
hakosaj <jussi.hakosalo@aalto.fi>
2023-07-20 15:33:36 +0100

    feat: creation of basic spectral dataset, pickling

--- processaudio.py
+++ processaudio.py
@@ -1,27 +1,39 @@
 import librosa
 import numpy as np
+import os
+import pickle
 
-dr=librosa.get_duration(filename="wav_files/afterglow.wav")
-y,sr = librosa.load("wav_files/afterglow.wav",sr=10000/dr)
+g=os.listdir("wav_files")
 
+spectrals=[]
+for vidfile in g:
 
-
-#Around 26k  points of sampled time serie data
-print(f"Sampling data: {y.shape}")
-#Sample rate, ere 100
-print(f"Sample rate: {sr}")
-mfccs = librosa.feature.mfcc(y=y, sr=sr)
-
-print(mfccs.shape)
-
-mfccs_mean = np.mean(mfccs, axis=0)
-mfccs_max = np.max(mfccs, axis=0)
-
-print(mfccs_max.shape)
-print(mfccs_mean)
-print(mfccs_max)
+    dr=librosa.get_duration(path=f"wav_files/{vidfile}")
+    y,sr = librosa.load(f"wav_files/{vidfile}",sr=10000/dr)
 
 
-#zero_crossings = librosa.zero_crossings(y[6000:6500], pad=False)
-#print(sum(zero_crossings))
-#https://towardsdatascience.com/extract-features-of-music-75a3f9bc265d
+
+    #25k  points of sampled time serie data
+    print(f"Sampling data: {y.shape}")
+    print(f"Sample rate: {sr}")
+    mfccs = librosa.feature.mfcc(y=y, sr=sr)
+
+    print(mfccs.shape)
+
+    mfccs_mean = np.mean(mfccs, axis=0)[::2]
+    mfccs_max = np.max(mfccs, axis=0)[::2]
+
+    print(mfccs_max.shape)
+    print(mfccs_mean.shape)
+
+
+    zero_crossings = librosa.zero_crossings(y, pad=False)
+    print(np.array((sum(zero_crossings))))
+    song_characteristics=np.concatenate((mfccs_mean,mfccs_max))
+    song_characteristics=np.concatenate((song_characteristics,[sum(zero_crossings)]))
+    spectrals.append(song_characteristics)
+    #https://towardsdatascience.com/extract-features-of-music-75a3f9bc265dnp.array(sum(zero_crossings))
+spectrals=np.array(spectrals)
+print(spectrals.shape)
+with open('spectrals.pickle', 'wb') as file:
+    pickle.dump(spectrals,file)

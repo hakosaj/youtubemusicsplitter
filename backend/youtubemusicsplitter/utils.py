ecdb3cae8535dff104b3e48266e68b7a48fea710
hakosaj <jussi.hakosalo@aalto.fi>
2023-09-02 16:58:27 +0100

    feat: playlist details send to frontend

--- backend/youtubemusicsplitter/utils.py
+++ backend/youtubemusicsplitter/utils.py
@@ -1,20 +1,37 @@
 import requests
 import json
+
+import os
+
+
+
 def get_playlist_from_url(url:str):
-    with open("ytapi.pub","r") as apifile:
-        api_key=apifile.read()
-    playlist_id=url.split("playlist?list=")[1]
-    playlist_items_url = f'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId={playlist_id}&key={api_key}'
-    playlist_items_data = requests.get(playlist_items_url).json()
-    playlist_data=json.dumps(playlist_items_data, indent=4)  # print the videos in the playlist
 
-    playlist_general_metadata_url=f'https://www.googleapis.com/youtube/v3/playlists?key={api_key}&id={playlist_id}&part=id,snippet&fields=items(id,snippet(title,channelId,channelTitle))'
-    playlist_general_metadata=requests.get(playlist_general_metadata_url).json()
-    playlist_general_metadata=json.dumps(playlist_general_metadata,indent=4)
+    try:
+        with open("youtubemusicsplitter/ytapi.pub","r") as apifile:
+            api_key=apifile.read()
+        playlist_id=url.split("playlist?list=")[1]
+        playlist_items_url = f'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId={playlist_id}&key={api_key}'
+        playlist_items_data = requests.get(playlist_items_url).json()
+        playlist_data=json.dumps(playlist_items_data, indent=4)  # print the videos in the playlist
 
-    playlist_metadata=json.loads(playlist_general_metadata)
-    playlist_name=playlist_metadata["items"][0]["snippet"]["title"]
-    playlist_channel=playlist_metadata["items"][0]["snippet"]["channelTitle"] 
+        playlist_general_metadata_url=f'https://www.googleapis.com/youtube/v3/playlists?key={api_key}&id={playlist_id}&part=id,snippet&fields=items(id,snippet(title,channelId,channelTitle))'
+        playlist_general_metadata=requests.get(playlist_general_metadata_url).json()
+        playlist_general_metadata=json.dumps(playlist_general_metadata,indent=4)
 
+        playlist_metadata=json.loads(playlist_general_metadata)
+
+        playlist_data=json.loads(playlist_data)
+
+        playlist_name=playlist_metadata["items"][0]["snippet"]["title"]
+        playlist_channel=playlist_metadata["items"][0]["snippet"]["channelTitle"]
+
+        
+        #Thumbnail of the first video
+        thumbnail_url=playlist_data["items"][0]["snippet"]["thumbnails"]["medium"]["url"]
+        playlist_length= len(playlist_data["items"])
+        
+        return playlist_data,playlist_name,playlist_channel, thumbnail_url, playlist_length
     
-    return playlist_data,playlist_name,playlist_channel
+    except Exception as e:
+        print(e)
--- backend/youtubemusicsplitter/views.py
+++ backend/youtubemusicsplitter/views.py
@@ -9,7 +9,13 @@ class StringView(APIView):
     def post(self, request):
         received_string = request.data.get('inputString', None)
         if received_string:
-            playlist_itemdata,playlist_name,playlist_channel=get_playlist_from_url(received_string)
+            try:
+                playlist_itemdata,playlist_name,playlist_channel, thumbnail_url, playlist_length=get_playlist_from_url(received_string)
 
-            return Response({"message": f"URL received successfully! Playlist in question: {playlist_name} by {playlist_channel}"}, status=status.HTTP_200_OK)
+                
+                return Response({"playlist_name": playlist_name, "playlist_channel": playlist_channel, "thumbnail_url":thumbnail_url, "playlist_length":playlist_length},status=status.HTTP_200_OK)
+            
+            except Exception as e:
+                return Response({"error": "Failed to fetch the playlist statistics"},status=status.HTTP_400_BAD_REQUEST)
+            
         return Response({"error": "No string provided."}, status=status.HTTP_400_BAD_REQUEST)
